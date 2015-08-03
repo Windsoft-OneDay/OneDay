f54d4beec3b067681531820bb6a9fc20de04681a
IronFactory <wind.dong.dream@gmail.com>
2015-08-04 00:28:53 +0900

    ver0

--- app/src/main/java/com/windsoft/oneday/fragment/SignupFragment.java
+++ app/src/main/java/com/windsoft/oneday/fragment/SignupFragment.java
@@ -21,15 +21,23 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
 
-        NumberPicker(rootView);
+        init(rootView);
 
         return rootView;
     }
 
-    private void NumberPicker(View rootview) {
-        year = (NumberPicker) rootview.findViewById(R.id.fragment_sign_up_year);
-        month = (NumberPicker) rootview.findViewById(R.id.fragment_sign_up_month);
-        day = (NumberPicker) rootview.findViewById(R.id.fragment_sign_up_day);
+    private void init(View rootView) {
+        setNumberPicker(rootView);
+    }
+
+
+    /**
+     * TODO; 생일 입력 NumberPicker 세팅
+     * */
+    private void setNumberPicker(View rootView) {
+        year = (NumberPicker) rootView.findViewById(R.id.fragment_sign_up_year);
+        month = (NumberPicker) rootView.findViewById(R.id.fragment_sign_up_month);
+        day = (NumberPicker) rootView.findViewById(R.id.fragment_sign_up_day);
 
         year.setMinValue(1950);
         year.setMaxValue(2015);
@@ -38,6 +46,5 @@ public class SignUpFragment extends Fragment {
         month.setMinValue(1);
         month.setMaxValue(12);
         month.setWrapSelectorWheel(false);
-
     }
 }
