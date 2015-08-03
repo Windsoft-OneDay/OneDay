08a607c237a83211691becd4078fa25cb82ea256
gkskzh100 <a22569447@gmail.com>
2015-08-04 00:23:49 +0900

    kim0.0.3
    SignUpFragment  Activity 에서 Fragment로 변경

--- app/src/main/java/com/windsoft/oneday/fragment/SignupFragment.java
+++ app/src/main/java/com/windsoft/oneday/fragment/SignupFragment.java
@@ -1,8 +1,10 @@
 package com.windsoft.oneday.fragment;
 
-
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.NumberPicker;
 
 import com.windsoft.oneday.R;
@@ -10,18 +12,24 @@ import com.windsoft.oneday.R;
 /**
  * Created by kim on 2015-08-03.
  */
-public class SignupFragment extends Activity {
+public class SignUpFragment extends Fragment {
     NumberPicker year;
     NumberPicker month;
     NumberPicker day;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_sign_up);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
 
-        year = (NumberPicker) findViewById(R.id.fragment_sign_up_year);
-        month = (NumberPicker) findViewById(R.id.fragment_sign_up_month);
-        day = (NumberPicker) findViewById(R.id.fragment_sign_up_day);
+        NumberPicker(rootView);
+
+        return rootView;
+    }
+
+    private void NumberPicker(View rootview) {
+        year = (NumberPicker) rootview.findViewById(R.id.fragment_sign_up_year);
+        month = (NumberPicker) rootview.findViewById(R.id.fragment_sign_up_month);
+        day = (NumberPicker) rootview.findViewById(R.id.fragment_sign_up_day);
 
         year.setMinValue(1950);
         year.setMaxValue(2015);
